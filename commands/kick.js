6c2eb1596a1f5aaa28ceb04cf668749c7fb02479
Sebo2205 <61531161+Sebo2205@users.noreply.github.com>
2020-08-31 17:12:00 -0400

    added kick command

---
+++ commands/kick.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: "kick",
+    usage: "kick <member:guildmember>",
+    requiredPermission: "commands.kick",
+
+    execute(message, args) {
+        const member = message.mentions.members.first();
+
+        if (member) {
+
+            var reason = args;
+
+            reason.shift();
+
+
+            
+            member.kick(reason.join(" ")).then(() => {
+                message.channel.send("succesfully kicked " + member.displayName + ", reason: " + reason.join(" "));
+            }).catch (error => {
+                throw error;
+            })
+
+        } else {
+            throw "you must mention a member";
+        }
+    }
+}
