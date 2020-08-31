66aedf3db0246300c8189d78b6911add2c62c5cd
Sebo2205 <61531161+Sebo2205@users.noreply.github.com>
2020-08-31 15:14:21 -0400

    Update cri.js

--- commands/cri.js
+++ commands/cri.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: "cri",
 
-    requiredPermission: "commands.cri",
 
     execute (message, args) {
         message.channel.send(message.author.username + " comitted ;-;");
