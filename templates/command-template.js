6c675cd5c276970b8516282cb40c747dbcb13ce5
Sebo2205 <61531161+Sebo2205@users.noreply.github.com>
2020-08-31 16:58:04 -0400

    more stuff

---
+++ templates/command-template.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "command-template",
+    description: "desription goes here, optional",
+    usage: "command usage goes here, optional",
+    requiredPermission: "command's required permission, optional",
+
+    execute(message, args) {
+        // command code goes here
+    }
+}
