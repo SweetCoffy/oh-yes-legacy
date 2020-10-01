3b1c21b280f29f471783a2e5ac36e483d6024805
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2020-10-01 16:21:58 -0700

    Update newport.js

--- commands/newport.js
+++ commands/newport.js
@@ -3,23 +3,18 @@ const stuff = require('../stuff');
 module.exports = {
     name: "newport",
     description: "just like announce but for server ports",
-    usage: "newport <game:string> <port:anyvalidportlollolol>",
+    usage: "newport <Game> | <Port>",
     requiredPermission: "commands.newport",
 
     execute(message, args, extraArgs) {
-        var game = args[0];
-        var port = args[1];
-        if (args.length < 2) throw "e";
-
-        var game2embedTitle = {
-            "terraria": "New terraria port",
-            "minecraft": "New minecraft port",
-            "mc": "New minecraft port",
-            "mcpe": "New minecraft port"
-        }
+        var newArgs = args.join(" ").trim().split("|")
         
+        var game = newArgs[0];
+        var port = newArgs[1];
+        if (newArgs.length != 2) throw "Invalid command usage Here is the proper one: \nnewport <Game> | <Port>";
+
         var embed = {
-            title: game2embedTitle[game] || "New port",
+            title: `New port for ${game}`,
             color: 0x3679e3,
             description: port,
         }
